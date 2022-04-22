ec85254835c4a782317fb0869562ca6c8fcb946a
JoannafKo <koj097@newschool.edu>
2022-04-22 13:35:08 -0400

    new

---
+++ README.md
@@ -0,0 +1,47 @@
+# Digital Diviniation - Zodiac
+A tool where users can enter in their birthday to learn more about their zodiac sign
+
+The website can be found [here]()
+
+The portfolio can be found [here](https://joanna-ko.glitch.me/p4.html)
+
+ ## About
+For this project I decided to do a project related to the western zodiac. Early in the process, I initially thought about creating an interactive route game, in which you can interact with and learn about a certain type of religion. I wasn't exactly sure which religion I would choose, but I knew that I would work with some type of mythology
+
+I decided to do something more general and anachronistic by choosing the western zodiac, which is mostly based in Greco-Roman mythology. During my research I discovered how the same zodiac is represented in different cultures. Like the Leo sign would be a crocodile in a different culture, instead of a lion. I found that to be fascinating, and I wanted to share it with both Zodiac enthusiasists and just normal people alike.
+
+ ## Process & Documentation
+I first got to started with the basics like the HTML, the style sheet, and the javascript. I got help from one of the DT tutors, Elena Pang, to guide me through creating the technical aspects of the projects. With her assistance, I created objects for the signs. I then created and objects with the names of the signs, with the properties including things such as "personality" and "element." I then did that 11 more times.
+
+<img src="Assets/WIP1.png" width="500"> 
+<img src="Assets/WIP2.png" width="300">
+
+I then created an input and button that has a calender built in. Using if else statements, I made it so that the signs matched their corresponding match and end dates, was able to express the properties through the paragraph tags. I added a few more details, like the sub heading titles to the properties, the text symbols for decor, and the links to the about and README pages
+
+<img src="Assets/WIP3.png" width="500">
+<img src="Assets/WIP5.png" width="500">
+<img src="Assets/WIP6.png" width="500">
+
+Once I completed the technical aspects of the website, I moved onto the cosmetic elements. I drew a starry background with photoshop. I wasn't satisfied with that, so I went to a video tutorial (linked down bellow), to create animating particles that were floating in the background. I used codepen to get the javascript, and then I tweeked that script so that it resembled more stars. I finally created an about page that summarized the digital diviniation tool. And that was about it.
+
+
+<img src="Assets/background.png" width="500">
+<img src="Assets/WIP8.png" width="500">
+
+
+
+## Challenges & Struggles
+The first challenge I faced were the objects and if /else statements. It was extremely difficult keeping up with the variables and documents, because they were usually off by several days or even a month. As shown in the console logs. It was fixed by making the the month variable get added by one.
+
+I ran into another challenge with the particles. Firstly, I had trouble getting the particles to extend to the bottom of the page (because my page had to be scrolled). No matter how much I try to change the canvas position, it rarely budged. Another challenge I encountered was with the margins. I had to create them so that the sentences were touching the edges of the screen, but it also caused the webs of the particles to appear if you hover the cursor over them. This error didn't bother me as much, but it would still be nice to now if I can fix it or not.
+
+<img src="Assets/WIP4.png" width="500">
+
+<img src="Assets/WIP9.png" width="500">
+<img src="Assets/WIP10.png" width="500">
+
+## References
+
+[Codepen](https://codepen.io/VincentGarreau/pen/bGxvQd)
+
+[Dynamic background tutorial](https://youtu.be/ELV32bqMBlc)
---
+++ main.js
@@ -0,0 +1,216 @@
+let zodiac = 
+{   'Aries':{
+        'dates':
+            [{"03": [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]}, 
+            {"04": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]}],
+        'You': "♈︎ Are an Aries ♈︎",
+        'Personality': "The ram is adventurous, bold, & energetic. But also short-tempered, impulsive, & stubborn",
+        'Element': "Fire",
+        'Backstory': "Aries is based on the mythological golden ram, who rescued Phrixus and Helle from their jealous stepmother. Aries can be different depending on the culture. In the Babylonian zodiac, it was a farmer",
+        'Jobs': "Daredevil, wrestler, stunt double, reality TV star"},
+    'Taurus':{
+        'dates':
+        [{"04": [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]},
+        {"05": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}],
+        'You': "♉︎ Are a Taurus ♉︎",
+        'Personality': "The bull is determined, stable, & gentle. But also stubborn, monotonous, & possessive",
+        'Element': "Earth",
+        'Backstory': "Taurus is based on the myth of Europa. Zeus transformed himself into a white bull to attract the princess Europa, who jumped on his back and rode with him all the way back to Crete",
+        'Jobs': "Banker, house spouse, conservationist, farmer"},
+    'Gemini':{
+        'dates': 
+            [{"05": [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]}, 
+            {"06": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]}],
+        'You': "♊︎ Are a Gemini ♊︎",
+        'Personality': "The twins are spontaneous, energetic, & curious. But also indecisive, anxious, & emotionally stunted",
+        'Element': "Air",
+        'Backstory': "Gemini is based on the mythological heroes, Castor and Pollux. Who, ironically, are not twins themselves, but became twins when joining in immortality. Gemini can be different depending on the culture. In the Babylonian zodiac, they are twin gods Lugal-irra and Meslamta-ea",
+        'Jobs': "Actor, social media personality, lawyer"},
+    'Cancer':{
+        'dates':
+        [{"06": [22, 23, 24, 25, 26, 27, 28, 29, 30]}, 
+        {"07": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 ,22]}],
+        'You': "♋︎ Are a Cancer ♋︎",
+        'Personality': "The crab is caring, receptive & domestic. But also moody, clingy, & insecure",
+        'Element': "Water",
+        'Backstory': "Cancer is based on Karkinos. The crab who, depending on the myth, got crushed under someone’s foot, only to get put into the sky as a constellation by Hera. Cancer can be different depending on the culture. In Egyptian astrology, is was depicted as a scarab, while in Mesopotamian astrology, it was a turtle",
+        'Jobs': "Babysitter, caretaker, teacher, therapist"},
+    'Leo':{
+        'dates': [{"07": [23, 24, 25, 26, 27, 28, 29, 30, 31]},
+        {"08": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]}],
+        'You': "♌︎ Are a Leo ♌︎",
+        'Personality': "The lion is passionate, creative, & bold. But also dramatic, arrogant, & vain",
+        'Element': "Fire",
+        'Backstory': "Leo is based on the Nemean lion. A beast that was killed and taxidermied by Heracles during his 12 Labors",
+        'Jobs': "Singer, actor, motivational speaker, director"},
+    'Virgo':{
+        'dates': [{"08": [23, 24, 25, 26, 27, 28, 29, 30, 31]},
+        {"09": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]}],
+        'You': "♍︎ Are a Virgo ♍︎",
+        'Personality': "The virgin is good at problem-solving, hardworking, & loyal. But also harsh, uptight, & a perfectionist",
+        'Element': "Earth",
+        'Backstory': "Virgo is heavily associated with Demeter & Persephone, but her myth is about a maiden. Virgo can be different depending on the culture. In the Babylonian zodiac, it was barley stalk to worship their fertility goddess Shala",
+        'Jobs': "Librarian, secretary, teacher, engineer"},
+    'Libra':{
+        'date': [{"09": [23, 24, 25, 26, 27, 28, 29, 30]},
+        {"10": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]}],
+        'You': "♎︎ Are a Libra ♎︎",
+        'Personality': "The balance is charming, diplomatic, & idealistic. But also gullible, gluttonous, & indecisive",
+        'Element': "Air",
+        'Backstory': "Libra is based on Themis, the titaness of justice and fairness, usually depicted holding the balanced scale and wearing a blindfold",
+        'Jobs': "Politician, social media manager, diplomat, motivational speaker"},
+    'Scorpio':{
+        'date': [{"10": [23, 24, 25, 26, 27, 28, 29, 30, 31]},
+        {"11": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]}],
+        'You': "♏︎ Are a Scorpio ♏︎",
+        'Personality': "The balance is charming, diplomatic, & idealistic. But also gullible, gluttonous, & indecisive",
+        'Element': "Water",
+        'Backstory': "Scorpio is based on the myth of Orion. While varying depending on retelling, it usually has a scorpion get sent to murder Orion for his actions, whether it be trying to hunt every animal on earth or romancing the hunting goddess Artemis",
+        'Jobs': "Politician, businessman, actor, radio personality, TV personality"},
+    'Sagittarius':{
+        'date': [{"11": [23, 24, 25, 26, 27, 28, 29, 30]},
+        {"12": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]}],
+        'You': "♐︎ Are a Sagittarius ♐︎",
+        'Personality': "The archer is adventurous, cheerful, & intelligent. But also impulsive, ignorant, and obsessive",
+        'Element': "Fire",
+        'Backstory': "Sagittarius is based on the mythological centaur, Chiron. The trainer and pioneer of many prominent heroes in Greek mythology, such as Achilles. Sagittarius can be different depending on the culture. In the Babylonian zodiac, he is the winged panther god, Nergal",
+        'Jobs': "Anthropologist, archeologist, party host, entertainer, clown"},
+    'Capricorn':{
+        'date': [{"12": [22, 23, 24, 25, 26, 27, 28, 29, 30, 31]},
+        {"01": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]}],
+        'You': "♑︎ Are a Capricorn ♑︎",
+        'Personality': "The sea goat is responsible, patient, & calm. But also fatalistic, insensitive, & apathetic",
+        'Backstory': "The mythological background of Capricorn is too vague. Sometimes it is associated with Amalthea, the nurturing mother goat of Zeus, or the goat god, Pan. Capricorn can be different depending on the culture. In the Hindu Zodiac, it was Makara, the mythological crocodile",
+        'Jobs': "Lawyer, accountant, principal, teacher, landlord"},
+    'Aquarius':{
+        'date': [{"01": [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]},
+        {"02": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 18]}],
+        'You': "♒︎ Are an Aquarius ♒︎",
+        'Personality': "The water-bearer is socially-conscious, innovative, & unique. But also provocative, short-tempered, & stubborn",
+        'Element': "Air",
+        'Backstory': "The mythological background of Aquarius is too vague. Sometimes it is associated with Deucalion, the Greek equivalent of Noah, the survivor of the deluge. Aquarius can be different depending on the culture. In the Babylonian zodiac, it was the god of creation and water, Enk",
+        'Jobs': "Artist, political commentator, revolutionary, whistleblower"},
+    'Pisces':{
+        'date': [{"02": [19, 20, 21, 22, 23, 24, 25, 26, 27, 28]},
+        {"03": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}],
+        'You': "♓︎ Are a Pisces ♓︎",
+        'Personality': "The fish are emotional, intuitive, & creative. But also malleable, inconsistent, and secretive",
+        'Element': "Water",
+        'Backstory': "The myth of Pisces involves Aphrodite and her son, Eros. In which the two transform into fish, or get rescued by fish, to escape the progenitor of monsters, Typhon. Pisces can be different depending on the culture. In Abrahamic religions, Pisces is a symbol of Jesus",
+        'Jobs': "Artist, writer, traveler"
+    }
+};
+
+let myZodiac;
+
+function userinput(){
+    let input = document.getElementById("userinput").value
+    let d = new Date(input);
+    let monthNumber = input.slice(5,7);
+    let monthDate = input.slice(8,10);
+    let month = d.getUTCMonth()+1;
+    let dates = d.getUTCDate();
+    if (month == 4){
+        if(dates <= 19){
+            myZodiac = "Aries"
+        }else{
+            myZodiac = "Taurus"
+        }
+    }
+    if(month == 5){
+        if(dates <= 20){
+            myZodiac = "Taurus"
+        }else{
+            myZodiac = "Gemini"
+        }
+    }
+    if(month == 6){
+        if(dates <= 21){
+            myZodiac = "Gemini"
+        }else{
+            myZodiac = "Cancer"
+        }
+    }
+    if(month == 7){
+        if(dates <= 22){
+            myZodiac = "Cancer"
+        }else{
+            myZodiac = "Leo"
+        }
+    }
+    if(month == 8){
+        if(dates <= 22){
+            myZodiac = "Leo"
+        }else{
+            myZodiac = "Virgo"
+        }
+    }
+    if(month == 9){
+        if(dates <= 22){
+            myZodiac = "Virgo"
+        }else{
+            myZodiac = "Libra"
+        }
+    }
+    if(month == 10){
+        if(dates <= 22){
+            myZodiac = "Libra"
+        }else{
+            myZodiac = "Scorpio"
+        }
+    }
+    if(month == 11){
+        if(dates <= 22){
+            myZodiac = "Scorpio"
+        }else{
+            myZodiac = "Sagittarius"
+        }
+    }
+    if(month == 12){
+        if(dates <= 21){
+            myZodiac = "Sagittarius"
+        }else{
+            myZodiac = "Capricorn"
+        }
+    }
+    if(month == 1){
+        if(dates <= 19){
+            myZodiac = "Capricorn"
+        }else{
+            myZodiac = "Aquarius"
+        }
+    }
+    if(month == 2){
+        if(dates <= 18){
+            myZodiac = "Aquarius"
+        }else{
+            myZodiac = "Pisces"
+        }
+    }
+    if(month == 3){
+        if(dates <= 20){
+            myZodiac = "Pisces"
+        }else{
+            myZodiac = "Aries"
+        }
+    }
+    console.log(dates);
+    console.log(month);
+    console.log(monthNumber);
+    console.log(monthDate);
+    console.log(input);
+    console.log(myZodiac);
+    console.log(zodiac[myZodiac].You);
+    document.getElementById("You").innerText = zodiac[myZodiac].You;
+    console.log(zodiac[myZodiac].Personality);
+    document.getElementById("Personality").innerText = zodiac[myZodiac].Personality;
+    console.log(zodiac[myZodiac].Element);
+    document.getElementById("Element").innerText = zodiac[myZodiac].Element;
+    console.log(zodiac[myZodiac].Backstory);
+    document.getElementById("Backstory").innerText = zodiac[myZodiac].Backstory;
+    console.log(zodiac[myZodiac].Jobs);
+    document.getElementById("Jobs").innerText = zodiac[myZodiac].Jobs;
+};
+
+
+document.getElementById("enterbtn").addEventListener("click", userinput);
