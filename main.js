e1ce78b6f6459037f41dedff32a13e6fa1851df3
JoannafKo <koj097@newschool.edu>
2022-05-04 03:17:06 -0400

    new

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ let zodiac =
         'Element': "Fire",
         'Backstory': "Aries is based on the mythological golden ram, who rescued Phrixus & Helle from their jealous stepmother. Aries can be different depending on the culture. In the Babylonian zodiac, it was a farmer",
         'Jobs': [{"Daredevil": "Your energetic nature will make for good entertainment"}, {"Wrestler": "Your energetic nature will make for good entertainment"}, {"Stunt Double": "Your bold nature will make for good entertainment"}, {"Athlete": "Your bold nature will make for good entertainment"}, {"Reality TV Star": "Your energetic nature will make for good entertainment"}],
-        'Gem' : [{"Bloodstone": "A stone that symbolizes courage & can protect the wearer from wounds"}, {"Jasper": "A stimulant that induces relaxation & stamina"}, {"Diamond": "A stone that symbolizes purity & innocent"}, {"Sapphire": "A magnet of opulence & royalty, can attract good fortune"}]},
+        'Gem' : [{"Bloodstone": "A stone that symbolizes courage & can protect the wearer from wounds"}, {"Jasper": "A stimulant that induces relaxation & stamina"}, {"Diamond": "A stone that symbolizes purity & innocent"}, {"Sapphire": "A magnet of opulence & royalty, can attract good fortune"}],
+        'Flower': [{"Daffodil": "This flower is quite the dichotomy, symbolizing both new beginnings & death, fitting for an Aries"}, {"Daisy": "Your innocence and purity makes you adventurous, yet impulsive, just like the daisy itself"}, {"Sweet Pea": "A flower of good luck and farewells, as an Aries, you’ll need a sweet pea"}],
+        'Compatibility':[],
+        'Incompatibility': []},
     'Taurus':{
         'dates':
         [{"04": [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]},
@@ -18,7 +21,10 @@ let zodiac =
         'Element': "Earth",
         'Backstory': "Taurus is based on the myth of Europa. Zeus transformed himself into a white bull to attract the princess Europa, who jumped on his back & rode with him all the way back to Crete",
         'Jobs': [{"Banker": "A Taurus is responsible enough to handle finances"}, {"House Spouse": "You must be dependable to be fit for this job, which as a Taurus, you are"}, {"Conservationist": "You must be dependable to be fit for this job, which as a Taurus, you are"}, {"Farmer": "It requires lots of determination & stability for this occupation"}, {"Financial Manager": "A Taurus is responsible enough to handle finances"}],
-        'Gem' : []},
+        'Gem' : [{"Sapphire": "A magnet of opulence & royalty, can attract good fortune"}, {"Emerald": "A stone that foresees the future, & brings good fortune"}, {"Agate": "A stone that rebalances your mind & body, it is associated with improvement & intelligence"}, {"Chrysoprase": "A stone that symbolizes business & money, may good fortune come to you"}],
+        'Flower': [{"Daisy": "Just like the Daisy, the Taurus is fertile, secretive, and loving"}, {"Lily of the Valley": "This flower is associated with purity and humbleness, just like the Taurus"}, {"Hawthorn": "This flower is a very hopeful one, and can be very helpful to the health of the Taurus"}],
+        'Compatibility':[{"Capricorn-Taurus": "You’re both gentle and reasonable, foundations for a good relationship"}, {"Taurus-Virgo": "A sense of justice and a sense of stability compliment each other very well"}, {"Cancer-Taurus": "You two are both caring and loving, and will protect each other no matter what"}],
+        'Incompatibility': [{"Leo vs. Taurus": "You will find each other way too different to even tolerate each other, one is too rowdy while the other is too grounded"}, {"Aquarius vs. Taurus": "You will find each other way too different to even tolerate each other, one is too wistful while the other is too grounded"}, {"Sagittarius vs. Taurus": "You will find each other way too different to even tolerate each other, one is too rowdy while the other is too grounded"}]},
     'Gemini':{
         'dates': 
             [{"05": [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]}, 
@@ -28,7 +34,10 @@ let zodiac =
         'Element': "Air",
         'Backstory': "Gemini is based on the mythological heroes, Castor & Pollux. Who, ironically, are not twins themselves, but became twins when joining in immortality. Gemini can be different depending on the culture. In the Babylonian zodiac, they are twin gods Lugal-irra & Meslamta-ea",
         'Jobs': [{"Actor": "Your spontaneous personality is inherently charismatic & perfect for diverse roles"}, {"Social Media Personality": "Your spontaneous personality will attract millions of followers"}, {"Lawyer": "Fitting for a Gemini’s spontaneous nature"}, {"PR Manager": "Your spontaneous personality enables you to control public opinion"}, {"Teacher": "Your charismatic personality allows you to share your knowledge with others"}],
-        'Gem' : []},
+        'Gem' : [{"Agate": "A stone that rebalances your mind & body, it is associated with improvement & intelligence"}, {"Emerald": "A stone that foresees the future, & brings good fortune"}, {"Pearl": "A stone that symbolizes the sea, prestige, wisdom, & sophistication"}, {"Turquoise": "A stone that protects luck & peace"}],
+        'Flower': [],
+        'Compatibility':[{"Gemini-Libra": "You two will see right through each other, what a wonderful pairing that would be"}, {"Gemini-Sagittarius": "Both of your spontaneity brings you closer together"}, {"Aquarius-Gemini": "Both of your spontaneity brings you closer together"}],
+        'Incompatibility': [{"Capricorn vs. Gemini": "You will find each other way too different to even tolerate each other, one is too rowdy while the other is too grounded"}, {"Gemini vs. Taurus": "You will find each other way too different to even tolerate each other, one is too rowdy while the other is too grounded"}, {"Gemini vs. Scorpio": "You two find each other two-faced and stuck-up, it’s not gonna work"}]},
     'Cancer':{
         'dates':
         [{"06": [22, 23, 24, 25, 26, 27, 28, 29, 30]}, 
@@ -38,7 +47,10 @@ let zodiac =
         'Element': "Water",
         'Backstory': "Cancer is based on Karkinos. The crab who, depending on the myth, got crushed under someone’s foot, only to get put into the sky as a constellation by Hera. Cancer can be different depending on the culture. In Egyptian astrology, is was depicted as a scarab, while in Mesopotamian astrology, it was a turtle",
         'Jobs': [{"Babysitter": "Your caring personality allows you to nurture children"}, {"Caretaker": "Your caring personality allows you to nurture other people"}, {"Teacher": "Your caring personality allows you to share your knowledge with others"}, {"Therapist": "Your caring personality allows you to analyze & treat others"}, {"Nurse": "Your caring personality allows you to nurture patient"}],
-        'Gem' : []},
+        'Gem' : [{"Moonstone": "A stone that symbolizes intuition, calmness, & beauty"}, {"Onyx": "A protector of stable unions & relationships"}, {"Ruby": "A stone that symbolizes love & raw emotion"}, {"Carnelian": "A stone that symbolizes courage, leadership, & motivation"}],
+        'Flower': [],
+        'Compatibility':[],
+        'Incompatibility': []},
     'Leo':{
         'dates': [{"07": [23, 24, 25, 26, 27, 28, 29, 30, 31]},
         {"08": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]}],
@@ -47,7 +59,10 @@ let zodiac =
         'Element': "Fire",
         'Backstory': "Leo is based on the Nemean lion. A beast that was killed *& taxidermied by Heracles during his 12 Labors",
         'Jobs': [{"Singer": "Your dramatic personality is inherently charismatic & have a powerful singing voice"}, {"Actor": "Your dramatic personality is inherently charismatic & perfect for diverse roles"}, {"Motivational Speaker": "Your dramatic personality is inherently charismatic & to uplift others"}, {"Director": "Your creative personality is allows you to create passionate works of filmography"}, {"Musician": "Your creative personality is allows you to create passionate works of discography"}],
-        'Gem' : []},
+        'Gem' : [{"Onyx": "A protector of stable unions & relationships"}, {"Sardonyx": "A stone that brings the wearer a better understanding & ability to communicate"}, {"Peridot": "A stone that protects the wearer of nightmares"}, {"Spinel": "A stone that helps the wearer become less egotistical"}],
+        'Flower': [],
+        'Compatibility':[],
+        'Incompatibility': []},
     'Virgo':{
         'dates': [{"08": [23, 24, 25, 26, 27, 28, 29, 30, 31]},
         {"09": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]}],
@@ -56,7 +71,10 @@ let zodiac =
         'Element': "Earth",
         'Backstory': "Virgo is heavily associated with Demeter & Persephone, but her myth is about a maiden. Virgo can be different depending on the culture. In the Babylonian zodiac, it was barley stalk to worship their fertility goddess Shala",
         'Jobs': [{"Librarian": "Your hardwork & knowledge gives you access to share so many books"}, {"Secretary": "Your loyal & hardworking nature makes you great for keeping correspondence & records"}, {"Therapist": "Your caring personality allows you to analyze & treat others"}, {"Teacher": "Your problem-solving personality allows you to share your knowledge with others"}, {"Investor": "You know where to put your money thanks to your problem-solving skills"}],
-        'Gem' : []},
+        'Gem' : [{"Lapis Lazuli": "A stone that symbolizes truth & friendship"}, {"Chrysolite": "A stone of many meanings, some consider it a symbol of friendship, others with power"}, {"Sapphire": "A magnet of opulence & royalty, can attract good fortune"}, {"Topaz": "A stone that celebrates union & marriage"}],
+        'Flower': [],
+        'Compatibility':[],
+        'Incompatibility': []},
     'Libra':{
         'date': [{"09": [23, 24, 25, 26, 27, 28, 29, 30]},
         {"10": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]}],
@@ -65,7 +83,10 @@ let zodiac =
         'Element': "Air",
         'Backstory': "Libra is based on Themis, the titaness of justice & fairness, usually depicted holding the balanced scale & wearing a blindfold",
         'Jobs': [{"Politician": "You are charismatic & evil enough to professionally deal with politics"}, {"Social Media Personality": "Your charming personality will attract millions of followers"}, {"Diplomat": "Your communication skills make you fit to represent & peacemaker"}, {"Motivational Speaker": "Your charming personality is inherently charismatic & to uplift others"}, {"Caterer": "You’re convincing enough to plan good things to come for your clients"}],
-        'Gem' : []},
+        'Gem' : [{"Chrysolite": "A stone of many meanings, some consider it a symbol of friendship, others with power"}, {"Tourmaline": "A stone that symbolizes healing of relationships & money"}, {"Opal": "A stone symbolizes hope & sex"}, {"Aquamarine": "A stone that maintains youth, virility, & transformation"}],
+        'Flower': [],
+        'Compatibility':[],
+        'Incompatibility': []},
     'Scorpio':{
         'date': [{"10": [23, 24, 25, 26, 27, 28, 29, 30, 31]},
         {"11": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]}],
@@ -74,7 +95,10 @@ let zodiac =
         'Element': "Water",
         'Backstory': "Scorpio is based on the myth of Orion. While varying depending on retelling, it usually has a scorpion get sent to murder Orion for his actions, whether it be trying to hunt every animal on earth or romancing the hunting goddess Artemis",
         'Jobs': [{"Politician": "You are charismatic & evil enough to professionally deal with politics"}, {"Engineer": "Your problem-solving skills make you fit to design & build"}, {"Actor": "Your spontaneous personality is inherently charismatic & perfect for diverse roles"}, {"Radio Personality": "Your energetic personality makes you an amazing magnet of attention"}, {"TV Personality": "Your energetic personality makes you an amazing magnet of attention"}],
-        'Gem' : []},
+        'Gem' : [{"Beryl": "A stone thought to have healing powers"}, {"Opal": "A stone symbolizes hope & sex"}, {"Aquamarine": "A stone that maintains youth, virility, and transformation"}, {"Topaz": "A stone that celebrates union and marriage"}],
+        'Flower': [],
+        'Compatibility':[],
+        'Incompatibility': []},
     'Sagittarius':{
         'date': [{"11": [23, 24, 25, 26, 27, 28, 29, 30]},
         {"12": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]}],
@@ -83,7 +107,10 @@ let zodiac =
         'Element': "Fire",
         'Backstory': "Sagittarius is based on the mythological centaur, Chiron. The trainer & pioneer of many prominent heroes in Greek mythology, such as Achilles. Sagittarius can be different depending on the culture. In the Babylonian zodiac, he is the winged panther god, Nergal",
         'Jobs': [{"Anthropologist": "Your adventurous personality enables you to discover new creatures"}, {"Archeologist": "Your adventurous personality enables you to discover new creatures"}, {"Party Host": "Your cheerful personality makes you extremely fun & joyful to be around"}, {"Entertainer": "Your cheerful personality makes you extremely fun & joyful to be around"}, {"Clown": "Your cheerful personality makes you extremely fun & joyful to be around"}],
-        'Gem' : []},
+        'Gem' : [{"Citrine": "A stone that symbolizes prosperity & joy"}, {"Bloodstone": "A stone that symbolizes courage & can protect the wearer from wounds"}, {"Tanzanite": "A stone that balances heart & mind"}, {"Zircon": "A stone that strengthens the wearer physically & mentally"}],
+        'Flower': [],
+        'Compatibility':[],
+        'Incompatibility': []},
     'Capricorn':{
         'date': [{"12": [22, 23, 24, 25, 26, 27, 28, 29, 30, 31]},
         {"01": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]}],
@@ -92,7 +119,10 @@ let zodiac =
         'Element': "Earth",
         'Backstory': "The mythological background of Capricorn is too vague. Sometimes it is associated with Amalthea, the nurturing mother goat of Zeus, or the goat god, Pan. Capricorn can be different depending on the culture. In the Hindu Zodiac, it was Makara, the mythological crocodile",
         'Jobs': [{"Lawyer": "Fitting for a Capricorn’s competitive nature"}, {"Accountant": "It takes a responsible person to be financial responsible, which what a Capricorn is"}, {"Principal": "Your patient personality allows you to handle faculty & student bodies"}, {"Teacher": "Your patient personality allows you to share your knowledge with others"}, {"Landlord": "Your responsible nature allows you to manage money & people grateful"}],
-        'Gem' : []},
+        'Gem' : [{"Turquoise": "A stone that protects luck & peace"}, {"Zircon": "A stone that strengthens the wearer physically & mentally"}, {"Tanzanite": "A stone that balances heart & mind"}, {"Garnet": "A stone that symbolizes blood & vitality"}],
+        'Flower': [],
+        'Compatibility':[],
+        'Incompatibility': []},
     'Aquarius':{
         'date': [{"01": [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]},
         {"02": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 18]}],
@@ -101,7 +131,10 @@ let zodiac =
         'Element': "Air",
         'Backstory': "The mythological background of Aquarius is too vague. Sometimes it is associated with Deucalion, the Greek equivalent of Noah, the survivor of the deluge. Aquarius can be different depending on the culture. In the Babylonian zodiac, it was the god of creation & water, Enk",
         'Jobs': [{"Artist": "Your innovative nature allows you to create new projects that wow & amaze people"}, {"Political Commentator": "Your socially-conscious nature makes you want to voice your opinions on controversial matters"}, {"Revolutionary": "Your socially-conscious nature makes you want to take matters in your own hands"}, {"Whistleblower": "Your socially-conscious nature makes you want to take matters in your own hands"}, {"Photographer": "Your innovative nature allows you to take beautiful photos that wow & amaze people"}],
-        'Gem' : []},
+        'Gem' : [{"Garnet": "A stone that symbolizes blood & vitality"}, {"Amethyst": "A stone that symbolizes compassion & serenity"}, {"Zircon": "A stone that strengthens the wearer physically & mentally"}, {"Pearl": "A stone that symbolizes the sea, prestige, wisdom, & sophistication"}],
+        'Flower': [],
+        'Compatibility':[],
+        'Incompatibility': []},
     'Pisces':{
         'date': [{"02": [19, 20, 21, 22, 23, 24, 25, 26, 27, 28]},
         {"03": [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}],
@@ -110,7 +143,10 @@ let zodiac =
         'Element': "Water",
         'Backstory': "The myth of Pisces involves Aphrodite & her son, Eros. In which the two transform into fish, or get rescued by fish, to escape the progenitor of monsters, Typhon. Pisces can be different depending on the culture. In Abrahamic religions, Pisces is a symbol of Jesus",
         'Jobs': [{"Artist": "Your creative nature allows you to create new projects that wow & amaze people"}, {"Writer": "Your creative nature allows you to create new stories that wow & amaze people"}, {"Traveler": "You’re a go-with-the-flow type who enjoys going where your heart yearns for"}, {"Doctor": "Your intuitive nature allows you to find your patients’ issues"}, {"Psychologist": "Your intuitive nature allows you to find your patients’ issues"}],
-        'Gem' : []}
+        'Gem' : [{"Amethyst": "A stone that symbolizes compassion & serenity"}, {"Zircon": "A stone that strengthens the wearer physically & mentally"}, {"Jasper": "A stimulant that induces relaxation & stamina"}, {"Aquamarine": "A stone that maintains youth, virility, and transformation"}],
+        'Flower': [],
+        'Compatibility':[],
+        'Incompatibility': []}
 };
 
 let myZodiac;
@@ -229,6 +265,10 @@ function userinput(){
 document.getElementById("enterbtn").addEventListener("click", userinput);
 document.getElementById("secondbutton").addEventListener("click", randomjob);
 document.getElementById("secondbutton").addEventListener("click", randomgem);
+document.getElementById("secondbutton").addEventListener("click", randomflower);
+document.getElementById("secondbutton").addEventListener("click", randomcompatibility);
+
+
 
 function randomjob(){
     document.getElementById("Jobs").innerText = Object.keys(zodiac[myZodiac].Jobs[Math.floor(Math.random()*5)])[0];
@@ -241,3 +281,15 @@ function randomgem(){
     document.getElementById("Gem").innerText = Object.keys(zodiac[myZodiac].Gem[Math.floor(Math.random()*4)])[0];
     document.getElementById("GemDescription").innerText = Object.values(zodiac[myZodiac].Gem[Math.floor(Math.random()*4)])[0];
 }
+
+
+function randomflower(){
+    document.getElementById("Flower").innerText = Object.keys(zodiac[myZodiac].Flower[Math.floor(Math.random()*3)])[0];
+    document.getElementById("FlowerDescription").innerText = Object.values(zodiac[myZodiac].Flower[Math.floor(Math.random()*3)])[0];
+}
+
+function randomcompatibility(){
+    document.getElementById("Compatibility").innerText = Object.keys(zodiac[myZodiac].Compatibility[Math.floor(Math.random()*3)])[0];
+    document.getElementById("CompatibilityDescription").innerText = Object.values(zodiac[myZodiac].Compatibility[Math.floor(Math.random()*3)])[0];
+}
+// Compatibility
